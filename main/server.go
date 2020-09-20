d10cdd34c70b67d6d4a0810f16612b1e5009c75d
Alexis <alexis.cauchois@hetic.net>
2020-09-20 16:45:26 +0200

    adding basic security to routes

--- main/server.go
+++ main/server.go
@@ -6,11 +6,24 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "Hello!")
-	})
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
 
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+
+	fmt.Fprintf(w, "Hello!")
+}
+
+
+func main() {
+	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8081\n")
 	if err := http.ListenAndServe(":8081", nil); err != nil {
